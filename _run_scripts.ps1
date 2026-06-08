8d60ff90d74a9e19ef3b3a9f2d8c0f0da047e052
fabagile <fabagile@outlook.fr>
2026-06-08 16:23:01 +0200

    wifi bat scripts moved into wifi_manager folder

--- _run_scripts.ps1
+++ _run_scripts.ps1
@@ -8,38 +8,35 @@ function Invoke-List {
     [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
     $listfile = ".\liste_{0}.txt" -F $Name
     $funcs = Get-Content -Path $listfile | ForEach-Object { $_.Trim() } | Where-Object { $_ -match '\S' }
+    
     $funcs | ForEach-Object {
         $dir = ".\{0}" -F $Name
-        $_
+        # $_
         $scriptPath = if ($_.Contains(".bat") ) {
             $dir
-            
         }
         else {
             Join-Path -Path $dir -ChildPath "$_.ps1"
-
         }
+
         "Lancement de {0}" -F $scriptPath
         
         $button = Read-Host "- V pour continuer (validate)`n- X pour passer`n- C pour annuler (cancel)"
 
         $continue = ($button.ToUpper()) -eq "V"
         $skip = ($button.ToUpper()) -eq "X"
-        # $cancel = ($button.ToUpper()) -eq "C"
+        $cancel = ($button.ToUpper()) -eq "C"
+        $button
 
-        # if ($cancel) {
-        #     "Processus annulés"
-        #     # Stop-Process Invoke-List
-        # }
+        if ($cancel) {
+            continue
+        }
         if ($continue) {
-            # "Lancement"
             & $scriptPath
         } 
         if ($skip) {
-            # "OK, je zappe"
-            continue
+            "Passage au processus suivant`n"
         }
 
-        # else {Stop-Process}
     }
 }
