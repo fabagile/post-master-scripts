1270408aacec552472c54ae9d8d8c8ce00fbf12f
fabagile <fabagile@outlook.fr>
2026-06-23 09:54:08 +0200

    run_scripts simplified & renamed to app

--- app.ps1
+++ app.ps1
@@ -7,24 +7,17 @@ function Invoke-List {
         # [string]$Extension
     )
     [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-    $listfile = ".\liste_{0}.txt" -F $Name
+    $listfile = ".\lists\{0}.txt" -F $Name
     $funcs = Get-Content -Path $listfile | ForEach-Object { $_.Trim() } | Where-Object { $_ -match '\S' }
     
-
+    
     $funcs | ForEach-Object {
-        $dir = ".\{0}" -F $Name
+        $dir = ".\src\{0}" -F $Name
         $scriptPath = Join-Path -Path $dir -ChildPath "$_.ps1"
-        # $_
-        # $bat
-        # $scriptPath = if ($_.Contains($bat) ) {
-        #     $dir2 = $dir.Split($bat)[0 ]
-        #     Join-Path -Path $dir2 -ChildPath "$.bat"
-        # }
-        # else {
-        # }
-
+        
         "Lancement de {0}" -F $scriptPath
         
+                
         $button = Read-Host "- V pour continuer (validate)`n- X pour passer`n- C pour annuler (cancel)"
 
         $continue = ($button.ToUpper()) -eq "V"
@@ -37,6 +30,7 @@ function Invoke-List {
         }
         if ($continue) {
             & $scriptPath
+            # $scriptPath
         } 
         if ($skip) {
             "Passage au processus suivant`n"
